65afad1bfd660f361970d3f7ad015e8412bc590f
xiaoming <844296970@qq.com>
2020-06-20 19:52:21 +0800

    Initial commit

---
+++ src/main/java/com/djm/seckill/App.java
@@ -0,0 +1,39 @@
+package com.djm.seckill;
+
+import com.djm.seckill.dao.UserDOMapper;
+import com.djm.seckill.dataobject.UserDO;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication(scanBasePackages = {"com.djm.seckill"})
+@RestController
+@MapperScan("com.djm.seckill")
+public class App {
+
+    @Autowired
+    private UserDOMapper userDOMapper;
+
+    @RequestMapping("/")
+    public String home() {
+        UserDO userDo = userDOMapper.selectByPrimaryKey(1);
+        if (userDo == null) {
+            return "用户对象不存在";
+        } else {
+            return  userDo.getName();
+        }
+    }
+
+    public static void main( String[] args ) {
+        System.out.println( "Hello World!" );
+        SpringApplication.run(App.class,args);
+    }
+}
